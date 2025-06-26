f5e255c4dd7c429c41c72567358a2e0b55944b1e
shresthasriv <shresthas04.ms@gmail.com>
2025-06-26 15:32:29 +0530

    added db connection file

---
+++ pkg/database/connection.go
@@ -0,0 +1,79 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"hospital-management-system/internal/config"
+	"hospital-management-system/internal/models"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+
+func Connect(cfg *config.Config) error {
+	dsn := cfg.Database.URL
+
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+		NowFunc: func() time.Time {
+			return time.Now().UTC()
+		},
+	})
+
+	if err != nil {
+		return fmt.Errorf("failed to connect to database: %w", err)
+	}
+
+	sqlDB, err := DB.DB()
+	if err != nil {
+		return fmt.Errorf("failed to get underlying sql.DB: %w", err)
+	}
+
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
+	log.Println("Database connected successfully")
+	return nil
+}
+
+func Migrate() error {
+	if DB == nil {
+		return fmt.Errorf("database connection not established")
+	}
+
+	err := DB.AutoMigrate(
+		&models.User{},
+		&models.Patient{},
+	)
+
+	if err != nil {
+		return fmt.Errorf("failed to run migrations: %w", err)
+	}
+
+	log.Println("Database migrations completed successfully")
+	return nil
+}
+
+func GetDB() *gorm.DB {
+	return DB
+}
+
+func Close() error {
+	if DB == nil {
+		return nil
+	}
+
+	sqlDB, err := DB.DB()
+	if err != nil {
+		return err
+	}
+
+	return sqlDB.Close()
+}
